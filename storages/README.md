cdfe4a7876280edaa6b8372421155e29d377eadc
Igor <zaqwes8811@github.com>
2012-12-14 21:55:04 +0300

    push

---
+++ storages/README.md
@@ -0,0 +1,4 @@
+db-sbox
+=======
+
+Data bases sandbox
---
+++ storages/yaml/yaml-cpp/util/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: G:/github/storages/yaml/yaml-cpp/util
+# Build directory: G:/github/storages/yaml/yaml-cpp/util
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
