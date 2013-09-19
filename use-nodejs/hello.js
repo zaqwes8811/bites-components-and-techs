ca4e172fb7040c7efab87a033a6366b83809aa60
bb <igor.a.lugansky@gmail.com>
2013-09-19 20:38:13 +0400

    Refresh javascript classes knowleges.

--- use-nodejs/hello.js
+++ use-nodejs/hello.js
@@ -1,1 +1,60 @@
 console.log("Hello World");
+
+// Constructor
+function Apple (type) {
+    this.type = type;
+    this.color = "red";
+    this.getInfo = getAppleInfo;
+}
+ 
+// anti-pattern! keep reading...
+function getAppleInfo() {
+    return this.color + ' ' + this.type + ' apple';
+}
+
+var apple = new Apple('macintosh');
+apple.color = "reddish";
+console.log(apple.getInfo());
+
+// Better
+function AppleBetter (type) {
+  this.type = type;
+  this.color = "red";
+  this.getInfo = function () {
+    return this.color + ' ' + this.type + ' apple';
+  };
+}
+
+var apple = new AppleBetter('macintosh');
+apple.color = "reddish";
+console.log(apple.getInfo());
+
+// Prototype
+function AppleProto (type) {
+    this.type = type;
+    this.color = "red";
+}
+ 
+AppleProto.prototype.getInfo = function() {
+    return this.color + ' ' + this.type + ' apple';
+};
+
+var apple = new AppleProto('macintosh');
+apple.color = "reddish";
+console.log(apple.getInfo());
+
+
+// 2 WAY
+var apple = {
+    type: "macintosh",
+    color: "red",
+    getInfo: function () {
+        return this.color + ' ' + this.type + ' apple';
+    }
+}
+
+console.log(apple.getInfo());
+
+
+
+
