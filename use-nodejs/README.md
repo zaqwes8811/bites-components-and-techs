a56a77be15bfa9580ecbdc1c7c2385249b2d8913
bb <igor.a.lugansky@gmail.com>
2013-09-19 21:45:07 +0400

    fix

--- use-nodejs/README.md
+++ use-nodejs/README.md
@@ -1,6 +1,6 @@
 - Возможно пригодится для тестирования кода для GAE 
 и скриптов свервисов гугла.
-- Front-end
+- Front-end, но тут нужны швы для работы с DOM
 
 Google Closure
 `npm install nclosure`
--- use-nodejs/hello.js
+++ use-nodejs/hello.js
@@ -1,5 +1,12 @@
-// JQuery on Windows no work
-// Google Closure... есть nclosure, но с Windows похоже не работает. 
+// @JQuery on Windows no work
+// https://github.com/MatthewMueller/cheerio
+
+// @Google Closure... есть nclosure, но с Windows похоже не работает. Похоже работает
+// http://gatapia.github.io/nclosure/tutorial.html
+
+// @AngularJS
+// http://docs.angularjs.org/tutorial
+
 
 console.log("Hello World");
 
@@ -59,12 +66,53 @@ var apple = {
 console.log(apple.getInfo());
 
 
-// nclosure
-// You can now run the test just by executing this file
+// We are now in closure mode
 require('nclosure').nclosure();
-goog.require('goog.structs.Trie');
-var trie = new goog.structs.Trie();
-console.log(trie)
+
+// Lets declare our namespace 'demo.listfiles'.  Its always a good idea
+// to declare namespaces as polluting the global scope whilst not as
+// critical in the server is still a bad practice.
+goog.provide('demo.listfiles');
+
+// Import some utilities from the closure libs
+goog.require('goog.array');
+
+// Import the fs package from the node core libs.  Using the 'node.js'
+// goog.require rather than the node 'require' allows this code to be
+// compile checked.
+goog.require('node.fs');
+goog.require('node.path');
+
+/**
+  * @constructor
+  * @param {string} dir The directory to list
+  */
+demo.listfiles = function(dir) {
+  var files = [];
+  this.getFilesInDir_(files, dir);
+  files.sort();
+  console.log(files.join('\n'));
+};
+
+/**
+  * @private
+  * @param {Array.<string>} files
+  * @param {string} dir
+  */
+demo.listfiles.prototype.getFilesInDir_ = function(files, dir) {
+  var filesInDir = node.fs.readdirSync(dir);
+  goog.array.forEach(filesInDir, function(f) {
+    var path = node.path.resolve(dir, f);
+    if (node.fs.statSync(path).isDirectory()) {
+      this.getFilesInDir_(files, path);
+    } else {
+      files.push(path);
+    };
+  }, this);
+};
+
+// List the files in the current directory
+new demo.listfiles(__dirname);
 
 
 
