d6fded61b5188e3a400b8cbb7ce8a745c5747028
zaqwes <igor.a.lugansky@gmail.com>
2014-02-27 16:52:00 +0300

    save

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -1,7 +1,13 @@
-main = do 
-  putStrLn "What is 2 + 2?"
-  x <- readLn
-  if x == 4 then 
-    putStrLn "You're right!"
-  else 
-    putStrLn "You're wrong!"
+module Main where
+ 
+import qualified Data.Map as M
+ 
+errorsPerLine = M.fromList
+    [ ("Chris", 472), ("Don", 100), ("Simon", -5) ]
+ 
+main = do putStrLn "Who are you?"
+          name <- getLine
+          case M.lookup name errorsPerLine of
+              Nothing -> putStrLn "I don't know you"
+              Just n  -> do putStr "Errors per line: "
+                            print n
