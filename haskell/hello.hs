a7643f2a861ab35025790491dbad92472c5d8b18
zaqwes <igor.a.lugansky@gmail.com>
2014-02-27 16:30:10 +0300

    save and push

--- haskell/hello.hs
+++ haskell/hello.hs
@@ -1,1 +1,5 @@
-main = putStrLn "Hello, World!"
+main = do putStrLn "What is 2 + 2?"
+          x <- readLn
+          if x == 4
+              then putStrLn "You're right!"
+              else putStrLn "You're wrong!"
