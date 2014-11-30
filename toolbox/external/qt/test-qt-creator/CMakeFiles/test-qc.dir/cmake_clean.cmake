b7c50abc037aacd78f6d087aac98009d98a4b10c
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-11-30 21:06:08 +0300

    add toolbox

---
+++ toolbox/external/qt/test-qt-creator/CMakeFiles/test-qc.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test-qc.dir/main.c.obj"
+  "test-qc.pdb"
+  "test-qc.exe"
+  "test-qc.exe.manifest"
+  "test-qc.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/test-qc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
